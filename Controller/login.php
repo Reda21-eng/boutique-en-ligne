d6d5a9a0b34becc17003e3eff020e5a72a02eaf2
anissa-ourdjini <anissa.ourdjini@laplateforme.io>
2025-04-29 14:13:54 +0200

    new admin

--- Controller/login.php
+++ Controller/login.php
@@ -12,16 +12,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $database = new Database();
         $db = $database->getConnection();
 
-        $stmt = $db->prepare("SELECT id, username, password FROM users WHERE email = ?");
+        $stmt = $db->prepare("SELECT id, username, password, role FROM users WHERE email = ?");
         $stmt->execute([$email]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
-            // Ajouter le rôle de l'utilisateur à la session
             $_SESSION['role'] = $user['role'];
-            header("Location: ../index.php");
+
+            // Redirection en fonction du rôle
+            if ($user['role'] === 'admin') {
+                header("Location: admin.php");
+            } else {
+                header("Location: ../index.php");
+            }
             exit();
         } else {
             $error = "Invalid email or password";
